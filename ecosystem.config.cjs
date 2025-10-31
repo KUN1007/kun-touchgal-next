8d4d4080613ac54d95895dfc9ceb7eb62d4e7004
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-10-31 23:50:14 +0800

    mod: PM2 deploy config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,10 +6,10 @@ module.exports = {
       name: 'kun-touchgal-next',
       port: 3000,
       cwd: path.join(__dirname),
-      instances: 1,
+      instances: 10,
       autorestart: true,
       watch: false,
-      max_memory_restart: '2G',
+      max_memory_restart: '1G',
       script: './.next/standalone/server.js',
       // https://nextjs.org/docs/pages/api-reference/config/next-config-js/output
       env: {

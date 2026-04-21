2434904db1b494831878a47a723f5414e9cb321d
Palentum <79781425+Palentum@users.noreply.github.com>
2026-04-21 16:19:11 +0800

    fix(app): prevent patch rating reassignment

--- validations/patch.ts
+++ validations/patch.ts
@@ -257,11 +257,32 @@ export const patchRatingCreateSchema = z.object({
     })
 })
 
-export const patchRatingUpdateSchema = patchRatingCreateSchema.merge(
-  z.object({
-    ratingId: z.coerce
-      .number({ message: 'patch rating ID 格式不正确' })
-      .min(1)
-      .max(9999999)
-  })
-)
+export const patchRatingUpdateSchema = z.object({
+  ratingId: z.coerce
+    .number({ message: 'patch rating ID 格式不正确' })
+    .min(1)
+    .max(9999999),
+  recommend: z
+    .string({ message: '推荐程度不正确' })
+    .refine((v) => KUN_GALGAME_RATING_RECOMMEND_CONST.includes(v as any), {
+      message: '推荐程度不正确'
+    }),
+  overall: z.coerce
+    .number({ message: '评分不正确' })
+    .min(1, { message: '评分最小为 1' })
+    .max(10, { message: '评分最大为 10' }),
+  playStatus: z
+    .string({ message: '游玩状态不正确' })
+    .refine((v) => KUN_GALGAME_RATING_PLAY_STATUS_CONST.includes(v as any), {
+      message: '游玩状态不正确'
+    }),
+  shortSummary: z
+    .string({ message: '简评不正确' })
+    .trim()
+    .max(1314, { message: '简评最多 1314 字' }),
+  spoilerLevel: z
+    .string({ message: '剧透等级不正确' })
+    .refine((v) => KUN_GALGAME_RATING_SPOILER_CONST.includes(v as any), {
+      message: '剧透等级不正确'
+    })
+})

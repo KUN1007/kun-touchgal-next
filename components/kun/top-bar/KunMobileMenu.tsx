5d733bfcf721568a1bb983d92e519332938fc97e
Palentum <79781425+Palentum@users.noreply.github.com>
2026-05-18 19:10:15 +0800

    style(top-bar): apply prettier formatting

--- components/kun/top-bar/KunMobileMenu.tsx
+++ components/kun/top-bar/KunMobileMenu.tsx
@@ -11,13 +11,7 @@ export const KunMobileMenu = () => {
     <NavbarMenu className="space-y-4">
       <NavbarMenuItem>
         <Link className="flex items-center" href="/">
-          <Image
-            src="/logo.webp"
-            alt=""
-            width={50}
-            height={50}
-            priority
-          />
+          <Image src="/logo.webp" alt="" width={50} height={50} priority />
           <p className="ml-4 mr-2 text-3xl font-bold">
             {kunMoyuMoe.creator.name}
           </p>
